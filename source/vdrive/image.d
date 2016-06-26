fc07c93b23ad3616831cae62cdb4d6851f388fae
Peter Particle <ParticlePeter@gmx.de>
2016-06-26 22:59:37 +0200

    Added veriations of createImageView

--- source/vdrive/image.d
+++ source/vdrive/image.d
@@ -19,6 +19,46 @@ struct Meta_Image {
 	VkImageViewCreateInfo	image_view_create_info;
 	VkMemoryRequirements	memory_requirements;
 	VkDeviceMemory			device_memory;
+
+	private bool			owns_device_memory = true;
+
+	// bulk destroy the resources belonging to this meta struct
+	void destroyResources() {
+		vk.device.vkDestroyImage( image, vk.allocator );
+		vk.device.vkDestroyImageView( image_view, vk.allocator );
+		if( owns_device_memory ) {
+			vk.device.vkFreeMemory( device_memory, vk.allocator );
+		}
+	}
+
+}
+
+
+/// create simple VkImage with one level and one layer, assume VK_IMAGE_TILING_OPTIMAL and VK_SHARING_MODE_EXCLUSIVE
+/// store vulkan data in argument meta image container, return container for piping 
+auto ref createImage( 
+	ref Meta_Image			meta,
+	VkFormat				image_format,
+	VkExtent2D				image_extent,
+	VkImageUsageFlags		image_usage,
+	VkSampleCountFlagBits	image_samples = VK_SAMPLE_COUNT_1_BIT ) {
+
+	VkImageCreateInfo depth_image_create_info = {
+		imageType				: VK_IMAGE_TYPE_2D,
+		format					: image_format,									// notice me senpai!
+		extent					: { image_extent.width, image_extent.height, 1 },
+		mipLevels				: 1,
+		arrayLayers				: 1,
+		samples					: image_samples,								// notice me senpai!
+		tiling					: VK_IMAGE_TILING_OPTIMAL,
+		usage					: image_usage,									// notice me senpai!
+		sharingMode				: VK_SHARING_MODE_EXCLUSIVE,
+		queueFamilyIndexCount	: 0,
+		pQueueFamilyIndices		: null,
+		initialLayout			: VK_IMAGE_LAYOUT_UNDEFINED,             		// notice me senpai!
+	};
+
+	return meta.createImage( depth_image_create_info );
 }
 
 
@@ -49,44 +89,39 @@ auto ref bindMemory( ref Meta_Image meta, VkMemoryPropertyFlags memory_property_
 
 /// create a VkImageView which closely coresponds to the underlying VkImage type
 /// store vulkan data in argument meta image container, return container for piping
-auto ref imageView( ref Meta_Image meta, VkImageSubresourceRange subresource_range ) {
-
-	meta.image_view_create_info.image				= meta.image;
-	meta.image_view_create_info.viewType			= cast( VkImageViewType )meta.image_create_info.imageType;
-	meta.image_view_create_info.format			= meta.image_create_info.format;
-	meta.image_view_create_info.subresourceRange	= subresource_range;
-	meta.image_view_create_info.components		= 
-		VkComponentMapping(
-			VK_COMPONENT_SWIZZLE_IDENTITY, 
-			VK_COMPONENT_SWIZZLE_IDENTITY, 
-			VK_COMPONENT_SWIZZLE_IDENTITY, 
-			VK_COMPONENT_SWIZZLE_IDENTITY 
-		);
-
-	vkCreateImageView( meta.device, &meta.image_view_create_info, meta.allocator, &meta.image_view ).vkEnforce;
-	return meta;
+auto ref imageView( ref Meta_Image meta, VkImageAspectFlags subrecource_aspect_mask ) {
+	VkImageSubresourceRange subresource_range = {
+		aspectMask		: subrecource_aspect_mask,
+		baseMipLevel	: cast( uint32_t )0,
+		levelCount		: meta.image_create_info.mipLevels,
+		baseArrayLayer	: cast( uint32_t )0,
+		layerCount		: meta.image_create_info.arrayLayers, };
+	return meta.imageView( subresource_range );
 }
 
+/// create a VkImageView which closely coresponds to the underlying VkImage type
+/// store vulkan data in argument meta image container, return container for piping
+auto ref imageView( ref Meta_Image meta, VkImageSubresourceRange subresource_range ) {
+	return meta.imageView( subresource_range, cast( VkImageViewType )meta.image_create_info.imageType, meta.image_create_info.format );
+}
 
-/// create typical depth buffer VkImage, store vulkan data in argument meta image container, return container for piping
-auto ref createDepthBufferImage( ref Meta_Image meta, VkFormat depth_image_format, VkExtent2D depth_image_extent ) {
+/// create a VkImageView with choosing a image view type and format for the underlying VkImage, component mapping is identity
+/// store vulkan data in argument meta image container, return container for piping
+auto ref imageView( ref Meta_Image meta, VkImageSubresourceRange subresource_range, VkImageViewType view_type, VkFormat format ) {
+	return meta.imageView( subresource_range, view_type, format, VkComponentMapping(
+		VK_COMPONENT_SWIZZLE_IDENTITY, VK_COMPONENT_SWIZZLE_IDENTITY, VK_COMPONENT_SWIZZLE_IDENTITY, VK_COMPONENT_SWIZZLE_IDENTITY ));
+}
 
-	VkImageCreateInfo depth_image_create_info = {
-		imageType				: VK_IMAGE_TYPE_2D,
-		format					: depth_image_format,						// notice me senpai!
-		extent					: { depth_image_extent.width, depth_image_extent.height, 1 },
-		mipLevels				: 1,
-		arrayLayers				: 1,
-		samples					: VK_SAMPLE_COUNT_1_BIT,						// notice me senpai!
-		tiling					: VK_IMAGE_TILING_OPTIMAL,
-		usage					: VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,	// notice me senpai!
-		sharingMode				: VK_SHARING_MODE_EXCLUSIVE,
-		queueFamilyIndexCount	: 0,
-		pQueueFamilyIndices		: null,
-		initialLayout			: VK_IMAGE_LAYOUT_UNDEFINED,             		// notice me senpai!
-	};
-
-	return meta.createImage( depth_image_create_info );
+/// create a VkImageView with choosing a image view type, format and VkComponentMapping for the underlying VkImage
+/// store vulkan data in argument meta image container, return container for piping
+auto ref imageView( ref Meta_Image meta, VkImageSubresourceRange subresource_range, VkImageViewType view_type, VkFormat format, VkComponentMapping component_mapping ) {
+	meta.image_view_create_info.image				= meta.image;
+	meta.image_view_create_info.viewType			= view_type;
+	meta.image_view_create_info.format				= format;
+	meta.image_view_create_info.subresourceRange	= subresource_range;
+	meta.image_view_create_info.components			= component_mapping;
+	vkCreateImageView( meta.device, &meta.image_view_create_info, meta.allocator, &meta.image_view ).vkEnforce;
+	return meta;
 }
 
 
