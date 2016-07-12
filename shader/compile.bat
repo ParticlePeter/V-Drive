3b90142a7b14cf56f42692aa5a5a46001e29eeb1
Peter Particle <ParticlePeter@gmx.de>
2016-07-12 12:40:35 +0200

    Moved main.d and shaders into a project vdrive-projects/build wich uses v-drive as sourceLIbrary

--- shader/compile.bat
+++
@@ -1,2 +0,0 @@
-glslangValidator -V -w -o "E:/Code/Vulkan/V-Drive/shader/simple_vert.spv" "E:/Code/Vulkan/V-Drive/shader/simple.vert"
-glslangValidator -V -w -o "E:/Code/Vulkan/V-Drive/shader/simple_frag.spv" "E:/Code/Vulkan/V-Drive/shader/simple.frag"
