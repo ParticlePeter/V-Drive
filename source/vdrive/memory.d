42a9b812c3692a960d2b9dc1797415ff120ea710
Peter Particle <ParticlePeter@gmx.de>
2016-07-20 14:57:50 +0200

    Added Meta_Memory struct for buffer and image independent device memory allocation, as well as cretaeMemory and initMemory

--- source/vdrive/memory.d
+++ source/vdrive/memory.d
@@ -10,6 +10,18 @@ import vdrive.buffer;
 import erupted;
 
 
+struct Meta_Memory {
+	mixin 					Vulkan_State_Pointer;
+	VkDeviceMemory			device_memory;
+	private VkDeviceSize	device_memory_size;
+
+	// bulk destroy the resources belonging to this meta struct
+	void destroyResources() {
+		vk.device.vkFreeMemory( device_memory, vk.allocator );
+	}
+}
+
+
 // memory_type_bits is a bitfield where if bit i is set, it means that the VkMemoryType i 
 // of the VkPhysicalDeviceMemoryProperties structure satisfies the memory requirements
 auto memoryTypeIndex(
@@ -47,5 +59,17 @@ auto allocateMemory( ref Vulkan vk, VkDeviceSize allocation_size, uint32_t memor
 	vkAllocateMemory( vk.device, &memory_allocate_info, vk.allocator, &device_memory ).vkEnforce;
 
 	return device_memory;
+}
+
+
+auto ref createMemory( ref Meta_Memory meta, VkDeviceSize allocation_size, uint32_t memory_type_index ) {
+	meta.device_memory = allocateMemory( *meta.vk, allocation_size, memory_type_index );
+	meta.device_memory_size = allocation_size;
+	return meta;
+}
+
+auto ref initMemory( ref Vulkan vk, VkDeviceSize allocation_size, uint32_t memory_type_index ) {
+	Meta_Memory meta = vk;
+	return meta.createMemory( allocation_size, memory_type_index );
 }
 
