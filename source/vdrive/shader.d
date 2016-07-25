fbb8fd428d466edb207042cd96282b47d70d1287
Peter Particle <ParticlePeter@gmx.de>
2016-07-25 18:57:01 +0200

    shader creatShaderModule accepts glsl files according to glslangValidator extension specification and compiles them if .spv does not exist or is not up to date

--- source/vdrive/shader.d
+++ source/vdrive/shader.d
@@ -10,8 +10,46 @@ import erupted;
 
 auto createShaderModule( ref Vulkan vk, string path ) {
 
+	import std.path : extension;
+	string ext = path.extension;
+	if( ext != ".spv" ) {
+
+		// convert to std.container.array!char
+		auto array = path.toStringz;					// this adds '\0' as last value and must be compensated in index calculations
+		array[ $ - ext.length - 1 ] = '_';				// substitute the . of .ext with _ to _ext
+
+		// append new extension .spv
+		string spv = ".spv";							// string with extension for memcpy
+		array.length = array.length + spv.length - 1;	// resize the array with the length of the new extension
+		array[ $-1 ] = '\0';							// set the last value to a terminating character
+		import core.stdc.string : memcpy;				// import and use memcopy to copy into the char array
+		memcpy( &array.data[ $ - spv.length ], spv.ptr, spv.length );
+
+		string spir_path = array.data.idup;				// create string from the char array
+		
+		import std.file : exists;
+		bool up_to_date = array.data.exists;			// check if the file exists
+
+		if( up_to_date ) {								// if file exists, check if it is up to date with the glsl source
+			import std.file : getTimes;					// need to get the modification times of the files
+			import std.datetime : SysTime;				// stored in comparable SysTimes
+			SysTime access_time, glsl_mod_time, spir_mod_time;	// access times are irrelevant
+			spir_path.getTimes( access_time, spir_mod_time );	// get spir-v file times (.spv)
+			path.getTimes( access_time, glsl_mod_time );		// get glsl file times
+			up_to_date = glsl_mod_time < spir_mod_time;			// set the up_to_date value if glsl is newer than spir-v
+		}
+
+		if( !up_to_date ) {
+			import std.process : execute;				// use process execute to call glslangValidator
+			string[6] compile_glsl_args = [ "glslangValidator", "-V", "-w", "-o", spir_path, path ];
+			auto compile_glsl = compile_glsl_args.execute;		// store in status struct
+			printf( compile_glsl.output.toStringz.ptr );		// print output
+		}
+		path = array.data.idup;							// create string from the composed char array
+	}
+
 	import std.stdio : File;
-	auto file = File( path ) ;
+	auto file = File( path );
 	auto read_buffer = sizedArray!char( cast( size_t )file.size );
 	auto code = file.rawRead( read_buffer.data );
 
