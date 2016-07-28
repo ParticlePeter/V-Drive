4376fec440c268fa2b0bf71e0dca0b41cb78f9a9
Peter Particle <ParticlePeter@gmx.de>
2016-07-28 21:23:38 +0200

    geometry removed input assembly specification, binding and attribute description functions from geometry, they belong and are in module pipeline.

--- source/vdrive/geometry.d
+++ source/vdrive/geometry.d
@@ -4,20 +4,20 @@ import core.stdc.stdio : printf;
 
 import vdrive.util;
 import vdrive.state;
-import vdrive.buffer;
+import vdrive.memory;
 
 import erupted;
 
 
 mixin template Meta_Geometry_Alias_This() {
-	this( ref Vulkan vk )	{	meta_geometry.meta_buffer.vk = &vk;  }
+	this( ref Vulkan vk )	{	meta_geometry.meta_buffer.vk = vk;  }
 	alias						meta_geometry this;
 	Meta_Geometry				meta_geometry;
 }
 
 
 struct Meta_Geometry {
-	this( ref Vulkan vk )			{  this.meta_buffer.vk = &vk;  }
+	this( ref Vulkan vk )			{  this.meta_buffer.vk = vk;  }
 	alias									meta_buffer this;
 	Meta_Buffer								meta_buffer;
 
@@ -40,24 +40,4 @@ auto ref initGeometry( ref Vulkan vk ) {
 	return Meta_Geometry( vk );
 }
 
-
-auto ref inputAssembly( ref Meta_Geometry meta, VkPrimitiveTopology primitive_topology, VkBool32 primitive_restart = VK_FALSE ) {
-	meta.input_assembly_create_info.topology = primitive_topology;
-	meta.input_assembly_create_info.primitiveRestartEnable = primitive_restart;
-	return meta;
-}
-
-auto ref bindingDescription( ref Meta_Geometry meta, size_t binding, size_t stride, VkVertexInputRate input_rate = VK_VERTEX_INPUT_RATE_VERTEX ) {
-	meta.binding_descriptions.append( VkVertexInputBindingDescription( binding.toUint, stride.toUint, input_rate ));
-	meta.vertex_input_create_info.pVertexBindingDescriptions = meta.binding_descriptions.ptr;
-	meta.vertex_input_create_info.vertexBindingDescriptionCount = meta.binding_descriptions.length.toUint;
-	return meta;
-}
-
-auto ref attributeDescription( ref Meta_Geometry meta, size_t location, size_t binding, VkFormat format, size_t offset = 0 ) {
-	meta.attribute_descriptions.append( VkVertexInputAttributeDescription( location.toUint, binding.toUint, format, offset.toUint ));
-	meta.vertex_input_create_info.pVertexAttributeDescriptions = meta.attribute_descriptions.ptr;
-	meta.vertex_input_create_info.vertexAttributeDescriptionCount = meta.attribute_descriptions.length.toUint;
-	return meta;
-}
 
