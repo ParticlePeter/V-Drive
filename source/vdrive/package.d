1d8261e73896b917f9fb3c26c1645ccf9dbc12d3
Peter Particle <ParticlePeter@gmx.de>
2016-10-07 15:10:18 +0200

    Added vdrive package module

---
+++ source/vdrive/package.d
@@ -0,0 +1,10 @@
+module vdrive;
+
+public import vdrive.util;
+public import vdrive.state;
+public import vdrive.memory;
+public import vdrive.shader;
+public import vdrive.surface;
+public import vdrive.geometry;
+public import vdrive.pipeline;
+public import vdrive.renderpass;
