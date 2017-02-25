9fcb1b4cdd862673c9ecac67c88b792ff1caa96a
Peter Particle <ParticlePeter@gmx.de>
2017-02-25 10:05:55 +0100

    util.util: restructured file, introduced nothrow:, removed old commented code

--- source/vdrive/util/util.d
+++ source/vdrive/util/util.d
@@ -4,13 +4,7 @@ import erupted;
 import std.container.array;
 
 
-
-alias vkMajor = VK_VERSION_MAJOR;
-alias vkMinor = VK_VERSION_MINOR;
-alias vkPatch = VK_VERSION_PATCH;
-
-
-
+/// check the correctness of a vulkan result
 void vkEnforce( VkResult vkResult ) {
 	import std.exception : enforce;
 	import std.conv : to;
@@ -18,20 +12,9 @@ void vkEnforce( VkResult vkResult ) {
 }
 
 
-alias toUint = toUint32_t;
-uint32_t toUint32_t( T )( T value ) if( __traits( isScalar, T )) { 
-	return cast( uint32_t )value;
-}
-
-alias toInt = toInt32_t;
-uint32_t toInt32_t( T )( T value ) if( __traits( isScalar, T )) { 
-	return cast( int32_t )value;
-}
-
-
-
+/// this is a general templated function to enumarate any vulkan property
+/// see usage in module surface or module util.info
 auto listVulkanProperty( ReturnType, alias vkFunc, Args... )( Args args ) {
-
 	import vdrive.util.array : ptr;
 	Array!ReturnType result;
 	VkResult vkResult;
@@ -61,6 +44,47 @@ auto listVulkanProperty( ReturnType, alias vkFunc, Args... )( Args args ) {
 	vkResult.vkEnforce; // check if everything went right
 
 	return result;
+}
+
+
+/// this is a general templated function to enumarate any vulkan property
+/// see usage in module surface or module util.info
+/// this overload takes a void* scratch space as first arg and does not allocate
+/// scratch memory needs to be sufficiently large, result will be cast and returned in this memory
+auto listVulkanProperty( ReturnType, alias vkFunc, Args... )( void* scratch, Args args ) {
+	import vdrive.util.array : ptr;
+	auto result = ( cast( ReturnType* )scratch )[ 0 .. 1 ];
+	VkResult vkResult;
+	uint32_t count;
+
+	do {
+		vkFunc( args, &count, null ).vkEnforce;
+		if( count == 0 )  break;
+		result = ( cast( ReturnType* )scratch )[ 0 .. count ];
+		vkResult = vkFunc( args, &count, &result[0] );
+	} while( vkResult == VK_INCOMPLETE );
+
+	vkResult.vkEnforce; // check if everything went right
+
+	return result;
+}
+
+
+nothrow:
+
+
+alias vkMajor = VK_VERSION_MAJOR;
+alias vkMinor = VK_VERSION_MINOR;
+alias vkPatch = VK_VERSION_PATCH;
+
+alias toUint = toUint32_t;
+uint32_t toUint32_t( T )( T value ) if( __traits( isScalar, T )) { 
+	return cast( uint32_t )value;
+}
+
+alias toInt = toInt32_t;
+uint32_t toInt32_t( T )( T value ) if( __traits( isScalar, T )) { 
+	return cast( int32_t )value;
 }
 
 
@@ -118,58 +142,3 @@ auto Forward_To_Inner_Struct( outer, inner, string path, ignore... )() {
 		}
 	} return result;
 }
-
-
-/+
-mixin template listVulkanTemplate( ReturnType, alias vkFunc, Args... ) {
-
-	import vdrive.util.array : ptr;
-	import std.container.array;
-	Array!ReturnType result;
-	VkResult vkResult;
-	uint32_t count;
-
-	/*
-	* It's possible, though very rare, that the number of
-	* instance layers could change. For example, installing something
-	* could include new layers that the loader would pick up
-	* between the initial query for the count and the
-	* request for VkLayerProperties. If that happens,
-	* the number of VkLayerProperties could exceed the count
-	* previously given. To alert the app to this change
-	* vkEnumerateInstanceExtensionProperties will return a VK_INCOMPLETE
-	* status.
-	* The count parameter will be updated with the number of
-	* entries actually loaded into the data pointer.
-	*/
-
-	do {
-		vkFunc( args, &count, null ).vkEnforce;
-		if( count == 0 )  break;
-		result.length = count;
-		vkResult = vkFunc( args, &count, result.ptr );
-	} while( vkResult == VK_INCOMPLETE );
-
-	vkResult.vkEnforce; // check if everything went right
-
-	//return listVulkanTemplate;
-}
-+/
-
-
-
-
-/*
-auto filterVulkanPropertyFlags(
-	Array!Queue_Family family_queues, 
-	VkQueueFlags include_queue, 
-	VkQueueFlags exclude_queue = 0 ) {
-
-	Array!Queue_Family filtered_queues;
-	foreach( ref family_queue; family_queues ) {
-		if(( family_queue.queueFlags & include_queue ) && !( family_queue.queueFlags & exclude_queue )) {
-			filtered_queues.insert( family_queue );
-		}
-	}
-	return filtered_queues;
-}*/
