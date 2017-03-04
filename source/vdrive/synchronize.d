1134d5cea0d6b9d8c9fb6c0913f900381f3bb8d4
Peter Particle <ParticlePeter@gmx.de>
2017-03-04 17:44:04 +0100

    synchronize: added module, convenient create functions for fence, semaphore and event with Vulkan state struct as parameter and optional flags for createFence

---
+++ source/vdrive/synchronize.d
@@ -0,0 +1,47 @@
+module vdrive.synchronize;
+
+import erupted;
+
+import vdrive.state;
+import vdrive.util.util : vkEnforce;
+
+
+///	create a VkFence
+///	Params:
+///		vk = reference to a VulkanState struct
+///		fence_create_flags = optional: only flag is VK_FENCE_CREATE_SIGNALED_BIT
+///	Returns: VkFence 
+auto createFence( ref Vulkan vk, VkFenceCreateFlags fence_create_flags = 0 ) {
+	VkFenceCreateInfo fence_create_info = { flags : fence_create_flags };
+	VkFence fence;
+	vkCreateFence( vk.device, &fence_create_info, vk.allocator, &fence ).vkEnforce;
+	return fence;
+}
+
+
+///	create a VkSemaphore
+/// the implicitly created VkSemaphoreCreateInfo has a flags member which currently must always be 0
+/// if this changes in a later release ( current v1.0.42 ) an optional parameter will be added
+///	Params:
+///		vk = reference to a VulkanState struct
+///	Returns: VkSemaphore 
+auto createSemaphore( ref Vulkan vk ) {
+	VkSemaphoreCreateInfo semaphore_create_info;
+	VkSemaphore semaphore;
+	vkCreateSemaphore( vk.device, &semaphore_create_info, vk.allocator, &semaphore ).vkEnforce;
+	return semaphore;
+}
+
+
+///	create a VkEvent
+/// the implicitly created VkEventCreateInfo has a flags member which currently must always be 0
+/// if this changes in a later release ( current v1.0.42 ) an optional parameter will be added
+///	Params:
+///		vk = reference to a VulkanState struct
+///	Returns: VkEvent 
+auto createEvent( ref Vulkan vk ) {
+	VkEventCreateInfo event_create_info;
+	VkEvent event;
+	vkCreateEvent( vk.device, &event_create_info, vk.allocator, &event ).vkEnforce;
+	return event;
+}
